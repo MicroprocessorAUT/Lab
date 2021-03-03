f9d6816ba7187f55ad13cbe60787154000d5958e
علی رضا صالحی <324@.>
2021-03-03 11:08:56 +0330

    Inheritance and virtual function sample improved

--- Samples/Intro/C++ Class in Arduino (Inheritance, Interfaces)/main/main.ino
+++ Samples/Intro/C++ Class in Arduino (Inheritance, Interfaces)/main/main.ino
@@ -1,69 +1,91 @@
 const int led1 = 6;
+const int led2 = 7;
 const int button1 = 12; // security alert blinking 
 const int button2 = 11; // normal alert blinkin
 
 
-class alert{    
+class Blinker{    
+  protected:
+    int ledPin = -1;
+    
+    void blinkBy(int ms) {
+      digitalWrite(ledPin, HIGH);
+      delay(ms);
+      digitalWrite(ledPin, LOW);
+      delay(ms);
+    }
+    
   public:
-  virtual void alertBlink();
-  virtual void off();
+    Blinker() { }
+    Blinker(int ledPin) {
+      this->ledPin = ledPin;
+    }
+   
+    void off(){
+      digitalWrite(ledPin , LOW);
+    }
+
+    virtual void alertBlink() { blinkBy(500); }
 };
 
 // two below classes extend from alert class
 
-class normalAlert : public alert{
-   public:
+class NormalBlinker : public Blinker{
+  public:
+    NormalBlinker(int ledPin) : Blinker(ledPin) { }
+    
     void alertBlink(){
-      digitalWrite(led1, HIGH);
-      delay(1000);
-      digitalWrite(led1, LOW);
+      Blinker::blinkBy(1000);
+    }
+};
+
+class SecurityBlinker : public Blinker{
+  public:
+    SecurityBlinker(int ledPin) : Blinker(ledPin) { }
+    
+    void alertBlink(){
+      Blinker::blinkBy(150);
+    }
+};
+
+class Alert {
+  private:
+    int inputPin = -1;
+    Blinker blinker;
+    
+  public:
+    Alert(int inputPin, Blinker blinker) {
+      this->inputPin = inputPin;
+      this->blinker = blinker;
     }
 
-    public:
-      void off(){
-        digitalWrite(led1 , LOW);
+    void watch() {
+      if (digitalRead(inputPin) == HIGH){
+        blinker.alertBlink();
       }
-  
-};
-
-class securityAlert : public alert{
-  public:
-    void alertBlink(){
-      digitalWrite(led1, HIGH);
-      delay(150);
-      digitalWrite(led1, LOW);
+      else {
+        blinker.off();
+      }
     }
-
-  public:
-    void off(){
-      digitalWrite(led1 , LOW);
-     }
-  
 };
-
-
   
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
 
 }
 
 void loop() {
-  normalAlert nA;
-  securityAlert sA;
-  if (digitalRead(button1) == HIGH){
-    sA.alertBlink();
-  }
+  NormalBlinker nB(led1);
+  SecurityBlinker sB(led2);
 
-  else if(digitalRead(button2) == HIGH){
-    nA.alertBlink();
-  }
-  else {
-    nA.off();
-    sA.off();
-  }
+  Alert sA(button1, sB);
+  Alert nA(button2, nB);
 
+  sA.watch();
+  nA.watch();
 }
